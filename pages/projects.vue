7dfccd492898276d2cc50abb53c84c5ec5db616c
PouyanNorouzi <irfn7pouyan@gmail.com>
2025-05-30 01:02:57 -0700

    added multicultural calender hosted link

--- pages/projects.vue
+++ pages/projects.vue
@@ -62,6 +62,7 @@ const projects = ref<Project[]>([
     image:
       "https://images.unsplash.com/photo-1557683316-973673baf926?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1129&q=80",
     github: "https://github.com/PouyanNorouzi/multicultural-calender",
+    liveDemo: "https://calendar.pouyannorouzi.com"
   },
   {
     id: 4,
