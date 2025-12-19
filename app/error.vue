a77672348b1612b406ba1fdb67baa264ecd7497c
PouyanNorouzi <irfn7pouyan@gmail.com>
2025-12-18 21:38:24 -0800

    added an error page

---
+++ app/error.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps<{
+  error: NuxtError;
+}>();
+</script>
+
+<template>
+  <UApp>
+    <AppHeader />
+    <UError :clear="{ class: 'rounded-full', icon: 'i-lucide-arrow-left' }" :error="error" />
+    <AppFooter />
+  </UApp>
+</template>
