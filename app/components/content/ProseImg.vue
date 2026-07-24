6523cf96508d8eafd26c26127f5f6a1eb57c84bb
PouyanNorouzi <irfn7pouyan@gmail.com>
2026-07-23 18:10:46 -0700

    added photos to fm blog

---
+++ app/components/content/ProseImg.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps({
+  src: {
+    type: String,
+    default: "",
+  },
+  alt: {
+    type: String,
+    default: "",
+  },
+  width: {
+    type: [String, Number],
+    default: undefined,
+  },
+  height: {
+    type: [String, Number],
+    default: undefined,
+  },
+});
+</script>
+
+<template>
+  <img
+    :src="src"
+    :alt="props.alt"
+    :width="props.width"
+    :height="props.height"
+    class="rounded-md will-change-transform cursor-zoom-in" />
+</template>
