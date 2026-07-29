2e2b0f21529f6238d565dd85ec8c3caf2a5be06a
PouyanNorouzi <irfn7pouyan@gmail.com>
2026-07-28 17:41:30 -0700

    reimplemented the zoom in on click for images in the markdown files and also centered the image

--- app/components/content/ProseImg.vue
+++ app/components/content/ProseImg.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from "vue";
+
 const props = defineProps({
   src: {
     type: String,
@@ -17,6 +19,15 @@ const props = defineProps({
     default: undefined,
   },
 });
+
+const open = ref(false);
+
+function onKeydown(e: KeyboardEvent) {
+  if (e.key === "Escape") open.value = false;
+}
+
+onMounted(() => window.addEventListener("keydown", onKeydown));
+onBeforeUnmount(() => window.removeEventListener("keydown", onKeydown));
 </script>
 
 <template>
@@ -25,5 +36,18 @@ const props = defineProps({
     :alt="props.alt"
     :width="props.width"
     :height="props.height"
-    class="rounded-md will-change-transform cursor-zoom-in" />
+    class="block mx-auto rounded-md will-change-transform cursor-zoom-in"
+    @click="open = true" />
+
+  <Teleport to="body">
+    <div
+      v-if="open"
+      class="fixed inset-0 z-50 flex items-center justify-center bg-black/80 backdrop-blur-sm cursor-zoom-out"
+      @click="open = false">
+      <img
+        :src="src"
+        :alt="props.alt"
+        class="max-w-[95vw] max-h-[95vh] object-contain rounded-md" />
+    </div>
+  </Teleport>
 </template>
